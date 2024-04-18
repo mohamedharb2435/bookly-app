a37f5c6590d1666e2bfc95e80d90afea0842a1a0
mohamedharb2435 <mkhaledharb@gmail.com>
2024-04-18 23:03:10 +0200

    created book details view

---
+++ lib/features/home/presentation/views/book-details-view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'widget/book-details-view-body.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:BookDetailsViewBody() ,
+    );
+  }
+}
