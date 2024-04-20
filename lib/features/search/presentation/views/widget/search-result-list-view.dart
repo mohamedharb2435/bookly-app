1a0c77ea28fa663d0b091ccebb86a5b85cbeffbe
mohamedharb2435 <mkhaledharb@gmail.com>
2024-04-20 21:56:56 +0200

    search-view-done

--- lib/features/search/presentation/views/widget/search-result-list-view.dart
+++ lib/features/search/presentation/views/widget/search-result-list-view.dart
@@ -1,26 +1,21 @@
+import 'package:bookly_app/features/home/presentation/views/widget/best-seller-list-view-iteam.dart';
 import 'package:flutter/material.dart';
 
-import 'best-seller-list-view-iteam.dart';
-
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
       padding: EdgeInsets.zero,
-      physics: const NeverScrollableScrollPhysics(),
       itemCount: 10,
       itemBuilder: (context, index) {
         return const Padding(
           padding: EdgeInsets.symmetric(vertical: 8),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: BookListViewItem(),
-          ),
+          child: BookListViewItem(),
         );
       },
-    );
+    );;
   }
 }
--- lib/features/search/presentation/views/widget/search-view-body.dart
+++ lib/features/search/presentation/views/widget/search-view-body.dart
@@ -1,5 +1,9 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widget/best-seller-list-view-iteam.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'custom-search-text-field.dart';
+import 'search-result-list-view.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -9,12 +13,22 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding:  EdgeInsets.symmetric(horizontal: 30),
       child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomSearchTextField()
+          CustomSearchTextField(),
+          SizedBox(height: 16,),
+          Text(
+            'Best Seller',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(height: 16,),
+          Expanded(child: SearchResultListView()),
+
         ],
       ),
     );
   }
 }
+
 
 
